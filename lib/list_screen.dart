5d3bce9edfc0a105955697e1e2275ac7c6c654be
Sandi Putra Sundawa <sundawasandi@gmail.com>
2022-07-06 16:37:20 +0700

    add bottom nav bar

--- lib/list_screen.dart
+++ lib/list_screen.dart
@@ -2,11 +2,27 @@ import 'package:flutter/material.dart';
 import 'package:flutter_demo/detail_screen.dart';
 import 'package:flutter_demo/identity.dart';
 
-class ListScreen extends StatelessWidget {
-  const ListScreen({super.key});
+class ListScreen extends StatefulWidget {
+  const ListScreen({Key? key}) : super(key: key);
+
+  @override
+  State<ListScreen> createState() => _ListScreenState();
+}
+
+class _ListScreenState extends State<ListScreen> {
+
+  int _selectedNavbar = 0;
+
+  void _changeSelectedNavBar(int index) {
+    setState(() {
+      _selectedNavbar = index;
+    });
+  }
+
 
   @override
   Widget build(BuildContext context) {
+
     List<Identity> dataIden = [];
     dataIden.add(Identity(
         nama: "Sandi",
@@ -24,7 +40,7 @@ class ListScreen extends StatelessWidget {
     return Scaffold(
         appBar: AppBar(
           title: const Text('List'),
-          leading: Icon(Icons.menu),
+          leading: const Icon(Icons.menu),
           actions: <Widget>[
             IconButton(
                 onPressed: () {
@@ -35,91 +51,115 @@ class ListScreen extends StatelessWidget {
                 onPressed: () {
                   _showToast(context, "Alarm");
                 },
-                icon: Icon(
+                icon: const Icon(
                   Icons.access_alarms,
                   color: Colors.white,
                 ))
           ],
         ),
-        body: SizedBox(
-          height: double.infinity,
-          child: ListView.builder(
-            itemBuilder: (context, index) {
-              return GestureDetector(
-                  onTap: () => {
-                        Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => const DetailScreen()))
-                      },
-                  child: Card(
-                    child: Padding(
-                      padding: const EdgeInsets.all(16),
-                      child: Column(
-                        children: <Widget>[
-                          Row(
-                            children: <Widget>[
-                              Container(
-                                width: 70,
-                                child: Text("Nama"),
-                              ),
-                              Container(
-                                  child: Flexible(
-                                child: Text(
-                                  ": ${dataIden[index].nama}",
-                                  maxLines: 2,
-                                  overflow: TextOverflow.visible,
-                                  style: const TextStyle(
-                                    color: Colors.blueGrey,
-                                  ),
+        bottomNavigationBar: BottomNavigationBar(
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+                icon: Icon(Icons.home), label: "Home", tooltip: "Home"),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.assessment),
+                label: "Assessment",
+                tooltip: "Assessment"),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.mail), label: "Mail", tooltip: "Mail"),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.settings),
+                label: "Settings",
+                tooltip: "Settings"),
+          ],
+          currentIndex: _selectedNavbar,
+          selectedItemColor: Colors.blueGrey,
+          unselectedItemColor: Colors.grey,
+          showUnselectedLabels: true,
+          onTap: _changeSelectedNavBar,
+        ),
+        body: Padding(
+          padding: EdgeInsets.all(16),
+          child: SizedBox(
+            height: double.infinity,
+            child: ListView.builder(
+              itemBuilder: (context, index) {
+                return GestureDetector(
+                    onTap: () => {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => const DetailScreen()))
+                        },
+                    child: Card(
+                      child: Padding(
+                        padding: const EdgeInsets.all(16),
+                        child: Column(
+                          children: <Widget>[
+                            Row(
+                              children: <Widget>[
+                                Container(
+                                  width: 70,
+                                  child: Text("Nama"),
                                 ),
-                              ))
-                            ],
-                          ),
-                          Row(
-                            children: <Widget>[
-                              const SizedBox(
-                                width: 70,
-                                child: Text("Alamat"),
-                              ),
-                              Container(
-                                  child: Flexible(
-                                child: Text(
-                                  ": ${dataIden[index].alamat}",
-                                  maxLines: 2,
-                                  overflow: TextOverflow.visible,
-                                  style: const TextStyle(
-                                    color: Colors.blueGrey,
+                                Container(
+                                    child: Flexible(
+                                  child: Text(
+                                    ": ${dataIden[index].nama}",
+                                    maxLines: 2,
+                                    overflow: TextOverflow.visible,
+                                    style: const TextStyle(
+                                      color: Colors.blueGrey,
+                                    ),
                                   ),
+                                ))
+                              ],
+                            ),
+                            Row(
+                              children: <Widget>[
+                                const SizedBox(
+                                  width: 70,
+                                  child: Text("Alamat"),
                                 ),
-                              ))
-                            ],
-                          ),
-                          Row(
-                            children: <Widget>[
-                              Container(
-                                width: 70,
-                                child: Text("Umur"),
-                              ),
-                              Container(
-                                  child: Flexible(
-                                child: Text(
-                                  ": ${dataIden[index].umur}",
-                                  maxLines: 2,
-                                  overflow: TextOverflow.visible,
-                                  style: const TextStyle(
-                                    color: Colors.blueGrey,
+                                Container(
+                                    child: Flexible(
+                                  child: Text(
+                                    ": ${dataIden[index].alamat}",
+                                    maxLines: 2,
+                                    overflow: TextOverflow.visible,
+                                    style: const TextStyle(
+                                      color: Colors.blueGrey,
+                                    ),
                                   ),
+                                ))
+                              ],
+                            ),
+                            Row(
+                              children: <Widget>[
+                                Container(
+                                  width: 70,
+                                  child: Text("Umur"),
                                 ),
-                              ))
-                            ],
-                          ),
-                        ],
+                                Container(
+                                    child: Flexible(
+                                  child: Text(
+                                    ": ${dataIden[index].umur}",
+                                    maxLines: 2,
+                                    overflow: TextOverflow.visible,
+                                    style: const TextStyle(
+                                      color: Colors.blueGrey,
+                                    ),
+                                  ),
+                                ))
+                              ],
+                            ),
+                          ],
+                        ),
                       ),
-                    ),
-                  ));
-            },
-            itemCount: dataIden.length,
+                    ));
+              },
+              itemCount: dataIden.length,
+            ),
           ),
         ));
   }
